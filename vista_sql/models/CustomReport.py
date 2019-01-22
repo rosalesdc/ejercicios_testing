e9bbaabcb51963bdf39cb4fd5ecd043866a8cee7
rosalesdc <rosales9146@gmail.com>
2019-01-22 15:39:16 -0600

    [REF] consulta-treeview trabaja para un innerJoin sencillo

--- vista_sql/models/CustomReport.py
+++ vista_sql/models/CustomReport.py
@@ -8,13 +8,24 @@ class CustomReport(models.Model):
     _description = "my report"
     _auto = False
 
-    name = fields.Char(string='Name', readonly=True)
-
+    name = fields.Char(string='Nombre', readonly=True)
+    partner_name = fields.Char(string='Nombre Partner', readonly=True)
+    
+#    def init(self):
+#        tools.drop_view_if_exists(self._cr, self._table)
+#        self._cr.execute("""CREATE or REPLACE VIEW my_report as  
+#                        SELECT 
+#                        id,
+#                        concat(name,' | ', amount_total) as name,
+#                        amount_total as amount_t
+#                        from purchase_order
+                        #""")
+    
     def init(self):
         tools.drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""CREATE or REPLACE VIEW my_report as  
-                        SELECT 
-                        id,
-                        concat(name,' | ', amount_total) as name
-                        from purchase_order
+                        SELECT purchase_order.id, purchase_order.name as name,res_partner.name as partner_name
+                        FROM purchase_order 
+                        INNER JOIN res_partner 
+                        ON purchase_order.partner_id=res_partner.id;
                         """)
