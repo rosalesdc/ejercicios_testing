5412e3e20923fecbab95b79a916afc9e1af07286
rosalesdc <rosales9146@gmail.com>
2019-05-20 17:10:25 -0500

    [REF] Soporte de sitio web

--- Academy/controllers/controllers.py
+++ Academy/controllers/controllers.py
@@ -2,7 +2,7 @@
 from odoo import http
 
 class Academy(http.Controller):
-    @http.route('/academy/academy/', auth='public')
+    @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw): #index nombre arbitrario de la función
         Teachers = http.request.env['academy.teachers']
         return http.request.render('Academy.index', { #nombre del módulo e index es por la página default
