d4737328afe2e98815aba376ff6569c8ddcce0de
rosalesdc <rosales9146@gmail.com>
2019-05-20 16:30:16 -0500

    [REF] Plantillas

--- Academy/controllers/controllers.py
+++ Academy/controllers/controllers.py
@@ -3,6 +3,8 @@ from odoo import http
 
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
-    def index(self, ** kw):
-        return "Hello, world"
+    def index(self, **kw): #index nombre arbitrario de la función
+        return http.request.render('Academy.index', { #nombre del módulo e index es por la página default
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
 
